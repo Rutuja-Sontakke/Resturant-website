2d1f88fd4752ae0a2be94af85ff171055ab3237b
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-07-31 15:12:06 +0530

    add getDashboard controller for fetching menu and reservation statistics

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,55 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import Menu from "../models/menu.model.js";
+import ApiError from "../utils/ApiError.js";
+import Reservation from "../models/reservation.model.js";
+
+
+const getDashboard = asyncHandler(async(req, res) => {
+    const[
+        totalMenuItems,
+        availableItems,
+        unavailableItems,
+        totalReservations,
+        pendingReservations,
+        confirmedReservations,
+        completeReservations,
+        cancelledReservations,
+    ] = await Promise.all([
+        Menu.countDocuments(),
+        Menu.countDocuments({ isAvailable: true}),
+        Menu.countDocuments({ isAvailable: false}),
+
+        Reservation.countDocuments(),
+        Reservation.countDocuments({ status: "Pending" }),
+        Reservation.countDocuments({ status: "Confirmed" }),
+        Reservation.countDocuments({ status: "Completed" }),
+        Reservation.countDocuments({ status: "Cancelled" }),
+    ]);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                menu: {
+                    total: totalMenuItems,
+                    available: availableItems,
+                    unavailable: unavailableItems,
+                },
+
+                reservations: {
+                    total: totalReservations,
+                    pending: pendingReservations,
+                    confirmed: confirmedReservations,
+                    completed: completedReservations,
+                    cancelled: cancelledReservations,
+                },
+            },
+
+            "Dashboard Fetched successfully"
+        )
+    )
+})
+
+
+export { getDashboard};
